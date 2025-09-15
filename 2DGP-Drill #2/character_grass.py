19ffd9f94a95d50ffc3563253bf0cd5edeb993dd
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-15 15:09:58 +0900

    2DGP Drill#2 - 2019180008 김상효 - 1

--- 2DGP-Drill #2/character_grass.py
+++ 2DGP-Drill #2/character_grass.py
@@ -6,37 +6,38 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-gab = 20
+gab = 50
 x = 400
 y = 9
 state = 0
 square = True
 radian = 0
+speed = 7.5
 
 def move_square():
     global  x,y,state,square
     if (state == 0):
-        x = x + 10
+        x = x + speed
         if(x >= 800 - gab):
             x = 800- gab
             state = 1
     elif (state == 1):
-        y = y + 10
+        y = y + speed
         if(y >= 600- gab):
             y = 600- gab
             state = 2
     elif (state == 2):
-        x = x - 10
+        x = x - speed
         if(x <= 0+ gab):
             x = 0+ gab
             state = 3
     elif (state == 3):
-        y = y - 10
+        y = y - speed
         if(y <= 0+ gab):
             y = 0+ gab
             state = 4
     elif (state == 4):
-        x = x+10
+        x = x+speed
         if(x >= 400):
             x = 400
             state = 0
@@ -48,13 +49,13 @@ def move_circle():
 
     x = math.cos(radian) * 390 + 400
     y = math.sin(radian) * 290 + 300
-    radian += 0.1
+    radian += 0.075
     if(radian >= 2*3.14):
         square = True
         radian = 0
         state = 0
         x = 400
-        y = 9
+        y =  gab
 
 
 while (True):
