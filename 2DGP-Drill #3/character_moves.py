98511054d5214321929f5ac37efc23142f30f555
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-16 18:03:07 +0900

    사각운동 만듦

--- 2DGP-Drill #3/character_moves.py
+++ 2DGP-Drill #3/character_moves.py
@@ -12,20 +12,41 @@ def render(x,y):
     delay(0.016)
 
 
+def move_bottomR():
+    for x in range(400,780,4):
+        render(x,40)
+
+def move_bottomL():
+    for x in range(20, 400,4):
+        render(x, 40)
+
+def move_top():
+    for x in range(780, 0,-4):
+        render(x, 560)
+
+def move_right():
+    for y in range(20, 560,4):
+        render(780, y)
+
+def move_left():
+    for y in range(580, 40,-4):
+        render(20, y)
+
+
 def move_rectangle():
     print('Moving rectangle')
-    render(400,300)
-    pass
-
+    move_bottomR()
+    move_right()
+    move_top()
+    move_left()
+    move_bottomL()
 
 def move_circle():
     print('Moving circle')
     for angle in range(0,360,1):
-        x = 400 + math.cos(math.radians(angle)) * 280
-        y = 300 + math.sin(math.radians(angle)) * 280
+        x = 400 + math.cos(math.radians(angle-90)) * 280
+        y = 300 + math.sin(math.radians(angle-90)) * 280
         render(x,y)
-    pass
-
 
 while(True):
     move_rectangle()
