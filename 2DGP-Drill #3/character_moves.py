6cbb20e2cd1b71e9434023ac379671dbdf00f0e8
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-16 15:18:19 +0900

    겹치는 부분 함수화

--- 2DGP-Drill #3/character_moves.py
+++ 2DGP-Drill #3/character_moves.py
@@ -6,20 +6,21 @@ open_canvas()
 boy = load_image('character.png')
 grass = load_image('grass.png')
 
+def render(x,y):
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(0.1)
+
 
 def move_rectangle():
     print('Moving rectangle')
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    render(400,300)
     pass
 
 
 def move_circle():
     print('Moving circle')
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    render(400, 300)
     pass
 
 
