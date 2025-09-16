72fbc907b8f47712003f40a36e15a4ffbe77dc88
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-16 15:16:47 +0900

    화면에 뜨는 지 확인

--- 2DGP-Drill #3/character_moves.py
+++ 2DGP-Drill #3/character_moves.py
@@ -8,10 +8,18 @@ grass = load_image('grass.png')
 
 
 def move_rectangle():
+    print('Moving rectangle')
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
 def move_circle():
+    print('Moving circle')
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(0.1)
     pass
 
 
