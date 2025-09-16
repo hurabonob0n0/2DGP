df09ee530ca89f5fe9c52911aa1b93146b556d9c
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-16 15:14:47 +0900

    기본 구조 만들기

--- 2DGP-Drill #3/character_moves.py
+++ 2DGP-Drill #3/character_moves.py
@@ -3,4 +3,18 @@ import math
 
 open_canvas()
 
-boy =
+boy = load_image('character.png')
+grass = load_image('grass.png')
+
+
+def move_rectangle():
+    pass
+
+
+def move_circle():
+    pass
+
+
+while(True):
+    move_rectangle()
+    move_circle()
