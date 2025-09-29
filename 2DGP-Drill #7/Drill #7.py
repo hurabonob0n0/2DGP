47047145f1a2e420618e3a7ccc43ea96d3997623
hurabonob0n0 <71112929+hurabonob0n0@users.noreply.github.com>
2025-09-29 14:03:28 +0900

    공 한 개 띄우기

--- 2DGP-Drill #7/Drill #7.py
+++ 2DGP-Drill #7/Drill #7.py
@@ -2,8 +2,35 @@ from pico2d import *
 
 open_canvas(400,800)
 
+class ball():
+    def __init__(self):
+        self.image = load_image('ball41x41.png')
+
+    def updaate(self):
+        pass
+
+    def render(self):
+        self.image.draw(100,100)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN: # 키가 눌렸을 때
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+running = True
+balls = [ball() for _ in range(5)]
+
 while running:
-    update()
-    render()
+    handle_events()
+    clear_canvas()
+    for ball in balls:
+        ball.render()
+    update_canvas()
+    delay(0.016)
 
 close_canvas()
